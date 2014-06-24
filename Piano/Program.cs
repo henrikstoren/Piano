a79d5c8b784b655d15dfbeb3b3f4b8d162250ea2
Henrik <henrikstoren@hotmail.com>
2014-06-24 14:13:09 +0200

    så langt jeg kom med keyevents

--- Piano/Program.cs
+++ Piano/Program.cs
@@ -5,84 +5,162 @@ using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using Toub.Sound.Midi;
+using System.Windows.Forms;
 
 namespace Piano
 {
-    internal class Program
+    internal class Program : Form
     {
-        private static void Main(string[] args)
+
+        public event KeyEventHandler KeyDown;
+
+        public Program()
         {
             MidiPlayer.OpenMidi();
             ConsoleKeyInfo k;
+            MidiPlayer.Play(new ProgramChange(0, 0, GeneralMidiInstruments.PanFlute));
+            string note = "C0";
+
             while (true)
             {
+                this.KeyDown = new KeyEventHandler(TestKeyDown);
                 k = Console.ReadKey();
                 char c = k.KeyChar;
-                int a = (int) c;
-                int freq = a*70;
 
-                //dette er teit
-                //nei det er det ikke!!!!
-                //joså
+                
 
+                //TODO: Need to use KeyDown- and KeyUp-events instead. A note will start on down and stop on up.
 
-                // For å spille sorte tangenter skriver vi f.eks "C#5"
+                // To play black keys we write for intance "C#5",
+                // which will be the key imediatly to the right of "C5"
 
-                if (c == 'i')
+                if (c == 'e')
                 {
-                    MidiPlayer.Play(new NoteOn(0, 0, "B4", 127));
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "C4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+//                    System.Threading.Thread.Sleep(1000);
+//                    MidiPlayer.Play(new NoteOff(0, 0, "C4", 127));
                 }
-                if (c == 'o')
+                if (c == '4')
                 {
-                    MidiPlayer.Play(new NoteOn(0, 0, "C5", 127));
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "C#4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
                 }
-                if (c == 'p')
+                if (c == 'r')
                 {
-                    MidiPlayer.Play(new NoteOn(0, 0, "D5", 127));
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "D4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
                 }
-                if (c == 'u')
+                if (c == '5')
                 {
-                    MidiPlayer.Play(new NoteOn(0, 0, "A4", 127));
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "D#4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 't')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "E4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
                 }
                 if (c == 'y')
                 {
-                    MidiPlayer.Play(new NoteOn(0, 0, "G4", 127));
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "F4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
                 }
-
+                if (c == '7')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "F#4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 'u')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "G4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == '8')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "G#4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 'i')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "A4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == '9')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "A#4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 'o')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "B4";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 'p')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "C5";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == '+')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "C#5";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
+                if (c == 'å')
+                {
+                    MidiPlayer.Play(new NoteOff(0, 0, note, 127));
+                    note = "D5";
+                    MidiPlayer.Play(new NoteOn(0, 0, note, 127));
+                }
 
 
 
                 // Play a chord
 
-                if (c == 'r')
-
-                {
-                    MidiPlayer.Play(new NoteOn(0, 0, "C4", 127));
-                    MidiPlayer.Play(new NoteOn(0, 0, "F4", 127));
-                    MidiPlayer.Play(new NoteOn(0, 0, "A4", 127));
-                }
-                if(c =='t')
-                {
-                    MidiPlayer.Play(new NoteOn(0, 0, "G4", 127));
-                    MidiPlayer.Play(new NoteOn(0, 0, "B4", 127));
-                    MidiPlayer.Play(new NoteOn(0, 0, "D4", 127));
-                }
+//                if (c == 'r')
+//
+//                {
+//                    MidiPlayer.Play(new NoteOn(0, 0, "C4", 127));
+//                    MidiPlayer.Play(new NoteOn(0, 0, "F4", 127));
+//                    MidiPlayer.Play(new NoteOn(0, 0, "A4", 127));
+//                }
+//                if(c =='t')
+//                {
+//                    MidiPlayer.Play(new NoteOn(0, 0, "G4", 127));
+//                    MidiPlayer.Play(new NoteOn(0, 0, "B4", 127));
+//                    MidiPlayer.Play(new NoteOn(0, 0, "D4", 127));
+//                }
             }
 
+        }
 
+        private static void TestKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C)
+            {
+                MidiPlayer.Play(new NoteOn(0, 1, "C4", 127));
+            }
+        }
 
+        private static void Main(string[] args)
+        {
+            Application.Run(new Program());
 
-
-//                MidiPlayer.Play(new NoteOn(0, 15, "C4", 127));
-//                MidiPlayer.Play(new NoteOn(0, 15, "F4", 127));
-//                MidiPlayer.Play(new NoteOn(0, 15, "A4", 127));
-            // Wait x milliseconds before we stop the note.
-            // May not be useful when we want to play consecutive/simultanous notes.
-//                System.Threading.Thread.Sleep(5000);
-//                MidiPlayer.Play(new NoteOff(0, 15, "C4", 127));
-//                MidiPlayer.Play(new NoteOff(0, 15, "F4", 127));
-//                MidiPlayer.Play(new NoteOff(0, 15, "A4", 127));
-//                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            
         }
     }
 }
